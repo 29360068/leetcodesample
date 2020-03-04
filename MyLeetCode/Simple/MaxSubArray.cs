3cb962f3b66aac8f8447f5d368e314f8a2c15671
naj <naj@ramnova.com>
2020-03-04 15:17:28 +0800

    最大子序求和，暴力解决，性能很差

---
+++ MyLeetCode/Simple/MaxSubArray.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyLeetCode.Simple
+{
+    /// <summary>
+    /// 序号：53. 最大子序和
+    /// 难度：简单
+    /// 描述：给定一个整数数组 nums ，找到一个具有最大和的连续子数组（子数组最少包含一个元素），返回其最大和。
+    /// </summary>
+    public class MaxSubArray
+    {
+        public int Excution1(int[] nums)
+        {
+            if (nums.Length == 0) return 0;
+
+            var biggest = int.MinValue;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                var sumResult = 0;
+                for (int j = i; j < nums.Length; j++)
+                {
+                    sumResult += nums[j];
+
+                    if(biggest <= sumResult)
+                    {
+                        biggest = sumResult;
+                    }
+                }
+            }
+
+            return biggest;
+        }
+    }
+}
---
+++ MyLeetCodeTests/Simple/MaxSubArrayTests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MyLeetCode.Simple.Tests
+{
+    [TestClass()]
+    public class MaxSubArrayTests
+    {
+        [TestMethod()]
+        public void Excution1Test()
+        {
+            var func = new MaxSubArray();
+
+            Assert.AreEqual(6, func.Excution1(new[] { -2, 1, -3, 4, -1, 2, 1, -5, 4 }));
+
+            Assert.AreEqual(0, func.Excution1(new int[] { }));
+
+            Assert.AreEqual(-2, func.Excution1(new int[] { -2 }));
+
+            Assert.AreEqual(-1, func.Excution1(new int[] { -2, -1, -3 }));
+
+            Assert.AreEqual(5, func.Excution1(new int[] { 5, -2, -1, -3, 1 }));
+        }
+    }
+}
