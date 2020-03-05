c7d3a961d87e398db30cab9b205a3353e45ceab2
naj <naj@ramnova.com>
2020-03-05 15:13:46 +0800

    q.58

---
+++ MyLeetCode/Simple/LengthOfLastWord.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyLeetCode.Simple
+{
+    /// <summary>
+    /// 序号：58. 最后一个单词的长度
+    /// 难度：简单
+    /// 描述：给定一个仅包含大小写字母和空格 ' ' 的字符串 s，返回其最后一个单词的长度。
+    /// </summary>
+    public class LengthOfLastWord
+    {
+        public int Excution1(string s)
+        {
+            if (string.IsNullOrWhiteSpace(s)) return 0;
+
+            var firstNotEmptyIndex = s.Length;
+            var i = s.Length - 1;
+            for (; i >= 0; i--)
+            {
+                if(s[i] == ' ')
+                {
+                    if(firstNotEmptyIndex != s.Length)
+                    {
+                        break;
+                    }
+                }
+                else
+                {
+                    if(firstNotEmptyIndex == s.Length)
+                    {
+                        firstNotEmptyIndex = i;
+                    }
+                }
+            }
+
+            return firstNotEmptyIndex - i;
+        }
+
+        public int Excution2(string s)
+        {
+            if (string.IsNullOrWhiteSpace(s)) return 0;
+
+            var i = s.Length - 1;
+            var count = 0;
+            for (; i >= 0; i--)
+            {
+                if (s[i] != ' ')
+                {
+                    count++;
+                }
+                else if(count > 0)
+                {
+                    break;
+                }
+            }
+
+            return count;
+        }
+    }
+}
---
+++ MyLeetCodeTests/Simple/LengthOfLastWordTests.cs
@@ -0,0 +1,33 @@
+﻿using MyLeetCode.Simple;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MyLeetCode.Simple.Tests
+{
+    [TestClass()]
+    public class LengthOfLastWordTests
+    {
+        [TestMethod()]
+        public void Excution1Test()
+        {
+            var func = new LengthOfLastWord();
+
+            Assert.AreEqual(3, func.Excution1("   xxx   ppp    "));
+            Assert.AreEqual(5, func.Excution1("Hello World"));
+            Assert.AreEqual(2, func.Excution1("pp vv xx"));
+            Assert.AreEqual(0, func.Excution1(""));
+            Assert.AreEqual(3, func.Excution1("ppp"));
+        }
+
+        [TestMethod()]
+        public void Excution2Test()
+        {
+            var func = new LengthOfLastWord();
+
+            Assert.AreEqual(3, func.Excution2("   xxx   ppp    "));
+            Assert.AreEqual(5, func.Excution2("Hello World"));
+            Assert.AreEqual(2, func.Excution2("pp vv xx"));
+            Assert.AreEqual(0, func.Excution2(""));
+            Assert.AreEqual(3, func.Excution2("ppp"));
+        }
+    }
+}
