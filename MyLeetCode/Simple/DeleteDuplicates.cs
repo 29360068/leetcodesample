9b15a3d50cef2cd80b869e48c79637326d71443f
naj <naj@ramnova.com>
2020-03-09 14:49:12 +0800

    q.83

---
+++ MyLeetCode/Simple/DeleteDuplicates.cs
@@ -0,0 +1,39 @@
+﻿namespace MyLeetCode.Simple
+{
+    /// <summary>
+    /// 序号：83. 删除排序链表中的重复元素
+    /// 难度：简单
+    /// 描述：给定一个排序链表，删除所有重复的元素，使得每个元素只出现一次。
+    /// </summary>
+    public class DeleteDuplicates
+    {
+
+        public class ListNode
+        {
+            public int val;
+            public ListNode next;
+            public ListNode(int x) { val = x; }
+        }
+
+        public ListNode Excution1(ListNode head)
+        {
+            var root = head;
+            ListNode pre = null;
+            while(head != null)
+            {
+                if(pre == null || pre.val != head.val)
+                {
+                    pre = head;
+                }
+                else
+                {
+                    pre.next = head.next;
+                }
+
+                head = head.next;
+            }
+
+            return root;
+        }
+    }
+}
---
+++ MyLeetCodeTests/Simple/DeleteDuplicatesTests.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MyLeetCode.Simple.Tests
+{
+    [TestClass()]
+    public class DeleteDuplicatesTests
+    {
+        private bool AreaEqual(DeleteDuplicates.ListNode node1, DeleteDuplicates.ListNode node2)
+        {
+            while (node1 != null && node2 != null)
+            {
+                if (node1.val != node2.val)
+                {
+                    return false;
+                }
+
+                node1 = node1.next;
+                node2 = node2.next;
+            }
+
+            if (node1 == null && node2 == null)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        [TestMethod()]
+        public void Excution1Test()
+        {
+            var func = new DeleteDuplicates();
+
+            Assert.IsTrue(AreaEqual(new DeleteDuplicates.ListNode(1)
+            {
+                next = new DeleteDuplicates.ListNode(2)
+            },
+            func.Excution1(new DeleteDuplicates.ListNode(1)
+            {
+                next = new DeleteDuplicates.ListNode(1)
+                {
+                    next = new DeleteDuplicates.ListNode(2)
+                }
+            })));
+
+            Assert.IsTrue(AreaEqual(new DeleteDuplicates.ListNode(1)
+            {
+                next = new DeleteDuplicates.ListNode(2)
+                {
+                    next = new DeleteDuplicates.ListNode(3)
+                }
+            },
+            func.Excution1(new DeleteDuplicates.ListNode(1)
+            {
+                next = new DeleteDuplicates.ListNode(1)
+                {
+                    next = new DeleteDuplicates.ListNode(2)
+                    {
+                        next = new DeleteDuplicates.ListNode(3)
+                        {
+                            next = new DeleteDuplicates.ListNode(3)
+                        }
+                    }
+                }
+            })));
+        }
+    }
+}
