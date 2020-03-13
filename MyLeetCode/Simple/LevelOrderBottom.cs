5d022f6563f36eb10afdc3489e4558df4a48555d
naj <naj@ramnova.com>
2020-03-13 10:57:17 +0800

    add q.104 and 1.107

--- MyLeetCode/Simple/LevelOrderBottom.cs
+++ MyLeetCode/Simple/LevelOrderBottom.cs
@@ -4,6 +4,11 @@ using System.Text;
 
 namespace MyLeetCode.Simple
 {
+    /// <summary>
+    /// 序号：107. 二叉树的层次遍历 II
+    /// 难度：简单
+    /// 描述：给定一个二叉树，返回其节点值自底向上的层次遍历。 （即按从叶子节点所在层到根节点所在的层，逐层从左向右遍历）
+    /// </summary>
     public class LevelOrderBottom
     {
         public IList<IList<int>> Excution1(IsSameTree.TreeNode root)
