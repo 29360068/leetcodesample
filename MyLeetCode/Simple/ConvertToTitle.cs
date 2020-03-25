c3b0793e0eb0528660a0a8587a0c55d56f6d95ad
naj <naj@ramnova.com>
2020-03-25 19:54:52 +0800

    add q.168

---
+++ MyLeetCode/Simple/ConvertToTitle.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace MyLeetCode.Simple
+{
+    /// <summary>
+    /// 序号：168. Excel表列名称
+    /// 难度：简单
+    /// 描述：给定一个正整数，返回它在 Excel 表中相对应的列名称。
+    /// </summary>
+    public class ConvertToTitle
+    {
+        public string Excution1(int n)
+        {
+            if (n <= 0) return string.Empty;
+
+            var result = string.Empty;
+
+            do
+            {
+                var y = n % 26;
+                n = n / 26;
+                if (y > 0)
+                {
+                    result = (char)((y - 1) + 'A') + result;
+                }
+                else
+                {
+                    result = 'Z' + result;
+                    n--;
+                }
+
+            } while (n > 0);
+
+            return result;
+        }
+    }
+}
---
+++ MyLeetCodeTests/Simple/ConvertToTitleTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MyLeetCode.Simple.Tests
+{
+    [TestClass()]
+    public class ConvertToTitleTests
+    {
+        [TestMethod()]
+        public void Excution1Test()
+        {
+            var func = new ConvertToTitle();
+
+            Assert.AreEqual("ZY", func.Excution1(701));
+            Assert.AreEqual("AA", func.Excution1(27));
+            Assert.AreEqual("A", func.Excution1(1));
+            Assert.AreEqual("B", func.Excution1(2));
+            Assert.AreEqual("Z", func.Excution1(26));
+            Assert.AreEqual("ZZ", func.Excution1(702));
+            Assert.AreEqual("AAA", func.Excution1(703));
+        }
+    }
+}
